97ffdb72a9d7f4254a9f724a4ffc05ff1f2b8128
Anatoly-23411 <popov234111@gmail.com>
2024-09-05 20:58:49 +0500

    Изменен формат вывода курсаю Теперь выводится только два знака после запятой.

--- Obmen.py
+++ Obmen.py
@@ -10,12 +10,12 @@ def exchange():
 
     if code:
         try:
-            response = requests.get("https:/open.er-api.com/v6/latest/USD")
+            response = requests.get("https://open.er-api.com/v6/latest/USD")
             response.raise_for_status()
             data = response.json()
             if code in data["rates"]:
                 exchange_rate = data["rates"][code]
-                mb.showinfo("Курс обмена", f"Курс: {exchange_rate} {code} за один доллар")
+                mb.showinfo("Курс обмена", f"Курс: {exchange_rate:.2f} {code} за один доллар")
             else:
                 mb.showerror("Ошибка!", f"Валюта {code} не найдена!")
         except Exception as e:
