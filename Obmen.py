6501ef7ccbc08788fb2d37207c9c60d524439dcb
Anatoly-23411 <popov234111@gmail.com>
2024-09-06 12:36:33 +0500

    В метку добавили названия валют.

--- Obmen.py
+++ Obmen.py
@@ -1,9 +1,19 @@
 # from cProfile import label
+# from pickletools import name2i
+
 import requests
 import json
 from tkinter import *
 from tkinter import messagebox as mb
 from tkinter import ttk
+
+# from idna import check_label
+
+
+def update_c_label(event):
+    code = combobox.get()
+    name = cur[code]
+    c_label.config(text=name)
 # from bottle import response, delete
 # from PyInstaller.loader.pyiboot01_bootstrap import entry
 def exchange():
@@ -17,7 +27,8 @@ def exchange():
             data = response.json()
             if code in data["rates"]:
                 exchange_rate = data["rates"][code]
-                mb.showinfo("Курс обмена", f"Курс: {exchange_rate:.2f} {code} за один доллар")
+                c_name = cur[code]
+                mb.showinfo("Курс обмена", f"Курс: {exchange_rate:.2f} {c_name} за один доллар")
             else:
                 mb.showerror("Ошибка!", f"Валюта {code} не найдена!")
         except Exception as e:
@@ -25,6 +36,18 @@ def exchange():
     else:
         mb.showwarning("Внимание!", f"Введите код валюты!")
 
+cur = {
+    "RUB": 'Российский рубль',
+    "EUR": 'Евро',
+    "GBP": 'Бритоанский фунт стерлингов',
+    "JPY": 'Японская йена',
+    "CNY": 'Китайский юань',
+    "KZT": 'Казахский тенге',
+    "UZS": 'Узбекский сум',
+    "CHF": 'Швейцарский франк',
+    "AED": 'Дирхам ОАЭ',
+    "CAD": 'Канадский доллар'
+}
 
 window = Tk()
 window.title("Курсы обмена валют")
@@ -32,10 +55,14 @@ window.geometry("360x180")
 
 Label(text="Выберите код валюты").pack(padx=10, pady=10)
 
-cur = ["RUB", "EUR", "GBP", "JPY", "CNY", "KZT", "UZS", "CHF", "AED", "CAD"]
-combobox = ttk.Combobox(values=cur)
+# cur = ["RUB", "EUR", "GBP", "JPY", "CNY", "KZT", "UZS", "CHF", "AED", "CAD"]
+combobox = ttk.Combobox(values=list(cur.keys()))
 combobox.pack(padx=10, pady=10)
+combobox.bind("<<ComboboxSelected>>", update_c_label)
 
+
+c_label = ttk.Label()
+c_label.pack(padx=10, pady=10)
 
 # entry = Entry()
 # entry.pack(padx=10, pady=10)
